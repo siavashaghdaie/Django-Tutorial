17f1814990402d7f98ed75d2a15c07b45d485c30
siavash.aghdaie <siavash.aghdaie@gmail.com>
2024-10-31 12:19:24 +0330

    First commit

---
+++ review/reviewapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
+
+# rooms = [
+#     {'id':1, 'name':'Lets learn python'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developer'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render (request, 'reviewapp/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i 
+    context = {'room':room}
+    return render (request, 'reviewapp/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        # print(request.POST)
+        # this is how we can process the data manually
+        # request.POST.get('name')
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request, 'reviewapp/room_form.html', context)
+
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'reviewapp/room_form.html', context)
